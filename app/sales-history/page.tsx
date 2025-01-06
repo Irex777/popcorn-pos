d3314e71dbd3c2f31e50ec257d3d807c7757aed7
Anton Boyko <boyko.anton@gmail.com>
2025-01-06 22:16:10 +0100

    sales-history/page corrected path

--- app/sales-history/page.tsx
+++ app/sales-history/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SalesHistory from '../../components/SalesHistory';
+import SalesHistory from '../components/SalesHistory';
 
 const SalesHistoryPage = () => {
   return (
